12ef1fe6d96f2e8e2afef8945ce0aa0e6d7e72df
richardmachage <rchacha261@gmail.com>
2024-05-17 11:56:38 +0300

    shared preferences repo

---
+++ app/src/main/java/com/forsythe/pm/data/sharedPreferences/PreferencesRepo.kt
@@ -0,0 +1,24 @@
+package com.forsythe.pm.data.sharedPreferences
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferencesRepo(
+    context: Context
+) {
+    private val sharedPreferences : SharedPreferences = context.getSharedPreferences(PREFERENCES_FILE_KEY,Context.MODE_PRIVATE)
+    companion object{
+        private const val PREFERENCES_FILE_KEY = "com.forsythe.PM.PREFERENCES"
+    }
+
+    fun saveData(key:String, value:String){
+        with(sharedPreferences.edit()){
+            putString(key, value)
+            apply()
+        }
+    }
+
+    fun loadData(key:String):String?{
+        return sharedPreferences.getString(key,"")
+    }
+}
--- app/src/main/java/com/forsythe/pm/presentation/Screens/MainActivity/MainViewModel.kt
+++ app/src/main/java/com/forsythe/pm/presentation/Screens/MainActivity/MainViewModel.kt
@@ -3,6 +3,8 @@ package com.forsythe.pm.presentation.Screens.MainActivity
 import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.forsythe.pm.models.LoginCredentials
+import com.forsythe.pm.models.LoginResponse
 import com.forsythe.pm.models.RegisterRequest
 import com.forsythe.pm.models.RegisterResponse
 import com.forsythe.pm.network.ApiService
@@ -15,6 +17,7 @@ class MainViewModel : ViewModel() {
 
     var name = mutableStateOf("Richard")
     var responseMessage = mutableStateOf("response message")
+    var accessToken = mutableStateOf("Access token")
     private val apiService = RetrofitClient.getInstance().create(ApiService::class.java)
 
     fun registerUser(
@@ -44,7 +47,25 @@ class MainViewModel : ViewModel() {
         })
     }
 
-    fun logInUser(){
+    fun logInUser(
+        emailOrUsername: String ,
+        password: String
+    ){
+        val loginCredentials = LoginCredentials(username_or_email = emailOrUsername, password = password )
+        val call  = apiService.logInUser(loginCredentials)
 
+        call.enqueue(object : Callback<LoginResponse>{
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                if (response.isSuccessful && response.body() != null){
+                    val responseData = response.body()!!.data
+                    accessToken.value = responseData.access_token
+                }
+            }
+
+            override fun onFailure(p0: Call<LoginResponse>, p1: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
