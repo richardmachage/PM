3e0a41c7238e6504846ca498d90402783a94c935
richardmachage <rchacha261@gmail.com>
2024-05-18 20:42:50 +0300

    DI : App module

--- app/src/main/java/com/forsythe/pm/presentation/Screens/RegisterScreen/SignUpViewModel.kt
+++ app/src/main/java/com/forsythe/pm/presentation/Screens/RegisterScreen/SignUpViewModel.kt
@@ -1,7 +1,24 @@
 package com.forsythe.pm.presentation.Screens.RegisterScreen
 
+import android.content.Context
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
-class SignUpViewModel : ViewModel() {
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    @ApplicationContext private val context:Context
+) : ViewModel() {
+    var username = mutableStateOf("")
+    var email =  mutableStateOf("")
+    var phoneNumber = mutableStateOf("")
+    var password = mutableStateOf("")
+
+
 
 }
